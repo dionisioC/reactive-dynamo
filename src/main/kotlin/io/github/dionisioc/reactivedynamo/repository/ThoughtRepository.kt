9f18c192e737453efed8d7976472201a69fa14fd
Dionisio Cortés Fernández <cortesfernandezdionisio@gmail.com>
2024-05-02 13:05:47 +0200

    refactor: Remove non necessary type

--- src/main/kotlin/io/github/dionisioc/reactivedynamo/repository/ThoughtRepository.kt
+++ src/main/kotlin/io/github/dionisioc/reactivedynamo/repository/ThoughtRepository.kt
@@ -4,7 +4,6 @@ import io.github.dionisioc.reactivedynamo.entity.Thought
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbAsyncTable
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedAsyncClient
 import software.amazon.awssdk.enhanced.dynamodb.Key
 import software.amazon.awssdk.enhanced.dynamodb.model.PutItemEnhancedRequest
@@ -13,7 +12,7 @@ import software.amazon.awssdk.enhanced.dynamodb.model.UpdateItemEnhancedRequest
 @Repository
 class ThoughtRepository(dynamoDbEnhancedAsyncClient: DynamoDbEnhancedAsyncClient) {
 
-    private var table: DynamoDbAsyncTable<Thought> =
+    private var table =
         dynamoDbEnhancedAsyncClient.table(Thought.TABLE_NAME, Thought.TABLE_SCHEMA)
 
     fun save(thought: Thought): Mono<Thought> {
